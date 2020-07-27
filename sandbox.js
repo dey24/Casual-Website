c918badf9ebba55beed649348cd1fcd0fa701d03
iamchiranjitdey <chiranjitdey2000@gmail.com>
2020-07-27 20:22:40 +0530

    scrollintoview()

--- sandbox.js
+++ sandbox.js
@@ -10,4 +10,18 @@ document.querySelector("html").style.scrollBehavior = "smooth";
 setTimeout(() => {
     document.querySelector("html").style.scrollBehavior = "unset";
 },2000);
-})
+});
+
+
+
+// document.getElementById('#learnit').addEventListener('click',()=>{
+
+// })
+
+document.getElementById('discover').addEventListener('click', () => {
+    const learnItElement = document.getElementById('learnit');
+    console.log(learnItElement)
+    learnItElement.scrollIntoView({
+        behavior: 'smooth'
+    });
+});
